3a83940f17e6f2092724dc55c5396b7de9a492e7
Emrah Diril <emrah@diril.org>
2025-11-20 02:40:25 -0500

    Fix twice loading

--- partials/footer.php
+++ partials/footer.php
@@ -6,7 +6,7 @@
                 <a href="/privacy" class="text-decoration-none text-light px-1 py-2 rounded-2 transition-all">Privacy</a>
                 <a href="/terms" class="text-decoration-none text-light px-1 py-2 rounded-2 transition-all">Terms of Service</a>
             </div>
-            <p>🌘 Newmoon Software &copy; <?php echo date('Y'); ?>. All rights reserved.</p>
+            <p>🌘 Emrah Diril &copy; <?php echo date('Y'); ?>. All rights reserved.</p>
         </div>
     </div>
 </footer>
