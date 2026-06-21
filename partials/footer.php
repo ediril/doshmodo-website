17e15c2d58bdf12cf5758ba66db4dd3571130b2a
Emrah Diril <emrah@diril.org>
2026-06-21 06:07:15 -0400

    remove blog for now

--- partials/footer.php
+++ partials/footer.php
@@ -2,7 +2,6 @@
     <div class="container">
         <div class="footer-content d-flex flex-column align-items-center justify-content-center">
             <div class="footer-links d-flex justify-content-center align-items-center mb-1 flex-wrap gap-4">
-                <a href="/blog" class="text-decoration-none px-1 py-2">Blog</a>
                 <a href="/privacy" class="text-decoration-none px-1 py-2">Privacy</a>
                 <a href="/terms" class="text-decoration-none px-1 py-2">Terms</a>
             </div>
